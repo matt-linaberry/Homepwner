e51f06351b98dcba8f8e5b26bb77da89fbbc2ff8
Matt Linaberry <matt.linaberry@we-ics.com>
2014-09-22 21:30:58 -0400

    Initial Commit

---
+++ Homepwner/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Homepwner
+//
+//  Created by Matthew Linaberry on 9/22/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Homepwner/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Homepwner
+//
+//  Created by Matthew Linaberry on 9/22/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
