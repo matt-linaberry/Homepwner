4aad5553bde43a922802889fe12cfbebada7d82c
Matt Linaberry <matt.linaberry@we-ics.com>
2014-09-23 21:52:52 -0400

    Includes bronze and silver challenges

--- Homepwner/BNRItemStore.m
+++ Homepwner/BNRItemStore.m
@@ -11,10 +11,31 @@
 
 @interface BNRItemStore()
 @property (nonatomic) NSMutableArray *privateItems;
+@property (strong, nonatomic) NSMutableArray *allItemsOver50;
+@property (strong, nonatomic) NSMutableArray *allItemsUnder50;
 @end
 
 @implementation BNRItemStore
-
+-(NSMutableArray *) allItemsUnder50
+{
+    
+    if (!_allItemsUnder50)
+    {
+        _allItemsUnder50 = [[NSMutableArray alloc] init];
+    }
+    
+    return _allItemsUnder50;
+}
+-(NSMutableArray *) allItemsOver50
+{
+    
+    if (!_allItemsOver50)
+    {
+        _allItemsOver50 = [[NSMutableArray alloc] init];
+    }
+    
+    return _allItemsOver50;
+}
 +(instancetype) sharedStore
 {
     static BNRItemStore *sharedStore;
@@ -40,6 +61,8 @@
     if (self)
     {
         _privateItems = [[NSMutableArray alloc] init];
+        _allItemsOver50 = [[NSMutableArray alloc] init];
+        _allItemsUnder50 = [[NSMutableArray alloc] init];
     }
     return self;
 }
@@ -49,10 +72,29 @@
     return [self.privateItems copy];
 }
 
-- (BNRItem *) createItem
+-(BNRItem *) createItem
 {
     BNRItem *item = [BNRItem randomItem];
-    [self.privateItems addObject:item];
+    
+    if (item.valueInDollars > 50) {
+        
+        if ([self.allItemsOver50 count] == 0) { // If the itemsOver50 array currently has no items inside, add it to the privateItems array
+            [self.privateItems addObject:self.allItemsOver50];
+        }
+        
+        [self.allItemsOver50 addObject:item];
+        
+    } else {
+        
+        if ([self.allItemsUnder50 count] == 0) { // If the itemsUnder50 array currently has no items inside, add it to the privateItems array
+            [self.privateItems addObject:self.allItemsUnder50];
+        }
+        
+        [self.allItemsUnder50 addObject:item];
+        
+    }
+    
     return item;
 }
+
 @end
--- Homepwner/BNRItemsViewController.m
+++ Homepwner/BNRItemsViewController.m
@@ -34,22 +34,55 @@
 {
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
-}
 
-- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return [[[BNRItemStore sharedStore] allItems] count];
 }
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSArray *allItems = [[BNRItemStore sharedStore] allItems];
+    NSInteger lastSectionIndex = [tableView numberOfSections] - 1;
+    if (section == lastSectionIndex) {
+        return [allItems[section] count] + 1;
+    } else {
+        return [allItems[section] count];
+    }
+}
 
-- (UITableViewCell *)tableView:(UITableView *)tableView
-         cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    NSArray *allItems = [[BNRItemStore sharedStore] allItems];
+    if ([allItems[section] count] > 0) {
+        if (section == 0) {
+            return @"Items Over $50";
+        } else {
+            return @"Items Under $50";
+        }
+    } else {
+        return nil;
+    }
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Get a new or recycled cell
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
-    NSArray *items = [[BNRItemStore sharedStore] allItems];
-    BNRItem *item = items[indexPath.row];
-    cell.textLabel.text = [item description];
+    
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    NSArray *allItems = [[BNRItemStore sharedStore] allItems];
+    
+    NSInteger lastSectionIndex = [tableView numberOfSections] - 1;
+    NSInteger lastRowIndex = [tableView numberOfRowsInSection:lastSectionIndex] - 1;
+    
+    if ((indexPath.section == lastSectionIndex) && (indexPath.row == lastRowIndex)) {
+        cell.textLabel.text = @"No more items!";
+    } else {
+        BNRItem *item = allItems[indexPath.section][indexPath.row];
+        cell.textLabel.text = [item description];
+    }
     
     return cell;
 }
+
 
 @end
