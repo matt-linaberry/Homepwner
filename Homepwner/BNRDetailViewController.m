1d541fed217726169e857ccfc9f8a5ae8ae3cda5
Matt Linaberry <matt.linaberry@we-ics.com>
2014-10-17 22:39:47 -0400

    Up to the start of page 220

--- Homepwner/BNRDetailViewController.m
+++ Homepwner/BNRDetailViewController.m
@@ -8,15 +8,33 @@
 
 #import "BNRDetailViewController.h"
 #import "BNRItem.h"
-@interface BNRDetailViewController ()
+@interface BNRDetailViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
 @property (weak, nonatomic) IBOutlet UITextField *valueField;
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
 
 @end
 
 @implementation BNRDetailViewController
+- (IBAction)takePicture:(id)sender
+{
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else
+    {
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    imagePicker.delegate = self;
+    
+    [self presentViewController:imagePicker animated:YES completion:NULL];
+}
 
 - (void) viewWillAppear:(BOOL)animated
 {
